3aae90f2949a0fc320cfe38aa7d3c948d0d2d30e
nikhil <nikhilkoche777@gmail.com>
2024-01-23 02:01:45 -0500

    changes

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from langchain_community.document_loaders import YoutubeLoader
-data = YoutubeLoader.from_youtube_url(
-    "https://www.youtube.com/watch?v=QsYGlZkevEg", add_video_info=False
-)
-if len(data.load()) == 0:
-    print('the list is empty')
+from youtube_transcript_api import YouTubeTranscriptApi
+video_url = "https://www.youtube.com/watch?v=o3K_HbpWNpgs"
+try:
+    video_id = video_url.split("v=")[1]
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+    if transcript:
+        subtitles_text = " ".join([entry["text"] for entry in transcript])
+        print(subtitles_text)
+    else:
+        print("TS NA")
+except Exception as e:
+    print(f"An error occured:{str(e)}")
